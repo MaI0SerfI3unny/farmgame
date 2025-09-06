e49410d2a3b1337c821e5157e16886f53516ff7d
daniledkin <daniledkin@gmail.com>
2025-09-06 15:53:40 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/logo.png"
+import work from "./assets/work_tree.png"
+import style from './App.module.scss';
+import { useState } from "react";
 
 function App() {
+  const [balance, setBalance] = useState(300);
+  const [effects, setEffects] = useState([]);
+
+  const handleClick = () => {
+    setBalance(prev => prev + 0.1);
+  
+    const id = Date.now();
+    const offsetX = Math.floor(Math.random() * 40 - 20);
+  
+    setEffects(prev => [...prev, { id, value: 0.1, offsetX }]);
+  
+    setTimeout(() => {
+      setEffects(prev => prev.filter(e => e.id !== id));
+    }, 1000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.layout}>
+      <div className={style.layoutContainer}>
+        <div className={style.containerLogo}>
+          <img src={logo} alt='logo'/>
+          <h1>Конопляна Ферма</h1>
+        </div>
+
+        <div className={style.workWrapper}>
+          <img 
+            onClick={handleClick} 
+            className={style.work} 
+            src={work} 
+            alt="work tree"
+          />
+          {effects.map(e => (
+            <span 
+              key={e.id} 
+              className={style.effect} 
+              style={{ '--offsetX': `${e.offsetX}px` }}>+{e.value.toFixed(1)}
+            </span>
+          ))}
+        </div>
+
+        <div className={style.bottomPanel}>
+          <div>
+            <div className={style.profile}>
+              <div className={style.nickname}>👤 Farmer123</div>
+              <div className={style.balance}>💰 {balance.toFixed(1)}$</div>
+            </div>
+            <div className={style.menu}>
+              <button>🛒 Магазин</button>
+              <button>📦 Склад</button>
+              <button>🌱 Ферма</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
